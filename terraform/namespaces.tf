af9cef1488afd8cf488c433004d110bfa4d27956
Nathan Thomas <nwthomas@me.com>
2025-08-07 21:08:44 -0700

    Add new changes for Helm/TF/Argo-based architecture

--- terraform/namespaces.tf
+++ terraform/namespaces.tf
@@ -1,11 +1,45 @@
-resource "kubernetes_namespace" "portainer" {
-  metadata {
-    name = "portainer-namespace-app"
+locals {
+  environments = ["eng", "prd"]
+  namespaces = {
+    argocd        = "argocd"
+    ingress_nginx = "ingress-nginx"
+    monitoring    = "monitoring"
+    cert_manager  = "cert-manager"
   }
 }
 
 resource "kubernetes_namespace" "argocd" {
   metadata {
-    name = "argocd-namespace-app"
+    name = local.namespaces.argocd
+    labels = {
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+}
+
+resource "kubernetes_namespace" "environments" {
+  for_each = toset(local.environments)
+  
+  metadata {
+    name = each.key
+    labels = {
+      "app.kubernetes.io/managed-by" = "terraform"
+      "environment" = each.key
+    }
+  }
+}
+
+resource "kubernetes_namespace" "services" {
+  for_each = { for pair in setproduct(local.environments, keys(local.namespaces)) : "${pair[1]}-${pair[0]}" => {
+    env = pair[0]
+    namespace = local.namespaces[pair[1]]
+  } if pair[1] != "argocd" }
+
+  metadata {
+    name = "${each.value.namespace}-${each.value.env}"
+    labels = {
+      "app.kubernetes.io/managed-by" = "terraform"
+      "environment" = each.value.env
+    }
   }
 }
